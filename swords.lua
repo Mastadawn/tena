a4e8f6b4d9019bbfb42153c182863fc8b07129b9
Mastadawn <68205491+Mastadawn@users.noreply.github.com>
2022-07-02 00:36:29 -0400

    Create swords.lua

---
+++ swords.lua
@@ -0,0 +1,28 @@
+swords = {
+    ["wood_sword"] = {
+        sword = "wood_sword",
+        rank = 1
+    },
+    ["stone_sword"] = {
+        sword = "stone_sword",
+        rank = 2
+    },
+    ["iron_sword"] = {
+        sword = "iron_sword",
+        rank = 3
+    },
+    ["diamond_sword"] = {
+        sword = "diamond_sword",
+        rank = 4
+    },
+    ["emerald_sword"] = {
+        sword = "emerald_sword",
+        rank = 5
+    },
+    ["rage_blade"] = {
+        sword = "rage_blade",
+        rank = 6,
+    },
+}
+
+return swords
